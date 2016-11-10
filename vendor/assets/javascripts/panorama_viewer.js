f33dafa3fa2fd023a56fa24e9a12923380e0c347
Muhammad Khan <muhammadkh4n@gmail.com>
2016-11-10 16:08:50 +0500

    pano fix

--- vendor/assets/javascripts/panorama_viewer.js
+++ vendor/assets/javascripts/panorama_viewer.js
@@ -1,1 +1,206 @@
-!function(e){function n(e,t){var n,r,i=t;this.pause=function(){window.clearTimeout(n);i-=new Date-r};this.resume=function(){r=new Date;n=window.setTimeout(e,i)};this.resume()}function r(e){var t=e.changedTouches,n=t[0],r="";switch(e.type){case"touchstart":r="mousedown";break;case"touchmove":r="mousemove";break;case"touchend":r="mouseup";break;default:return}var i=document.createEvent("MouseEvent");var s=2;if(navigator.userAgent.match(/Android/i)){s=10}i.initMouseEvent(r,true,true,window,1,n.screenX,n.screenY,n.clientX*s,n.clientY*s,false,false,false,false,0,null);n.target.dispatchEvent(i)}var t={repeat:false,direction:"horizontal",animationTime:700,easing:"ease-out",overlay:true};e.fn.panorama_viewer=function(n){document.addEventListener("touchstart",r,true);document.addEventListener("touchmove",r,true);document.addEventListener("touchend",r,true);document.addEventListener("touchcancel",r,true);return this.each(function(){var r=e.extend({},t,n),i=e(this);i.find("> img").load(function(){function l(t){var n={x:false,y:false},i={x:a.x-(u.x-t.clientX),y:a.y-(u.y-t.clientY)};if(r.direction=="horizontal"){if(r.repeat==true){n.x=true}else{n.x=i.x<0&&i.x*-1<o.w}if(f&&n.x){a.x=i.x;a.y=0}}else{if(r.repeat==true){n.y=true}else{n.y=i.y<0&&i.y*-1<o.h}if(f&&n.y){a.y=i.y;a.x=0}}e(this).css("background-position",a.x+"px "+a.y+"px");u.x=t.clientX;u.y=t.clientY;t.stopPropagation();return false}function c(t){f=false;n.unbind("mousemove",l);if(t.type=="mousedown"){u.x=t.clientX;u.y=t.clientY;f=true;n.bind("mousemove",l)}else{e(document.body).focus()}t.stopPropagation();return false}function h(){a={x:0,y:0};e(this).css("backgroundPosition","0 0")}i.find("> img").addClass("pv-pano");i.addClass("pv-container").wrapInner("<div class='pv-inner pv-animating'></div>");if(r.direction=="vertical"){i.addClass("pv-vertical")}i.find(".pv-animating").css({"-webkit-transition":"all "+r.animationTime+"ms "+r.easing,"-moz-transition":"all "+r.animationTime+"ms "+r.easing,"-ms-transition":"all "+r.animationTime+"ms "+r.easing,transition:"all "+r.animationTime+"ms "+r.easing});imgSrc=i.find(".pv-pano").attr("src");width=i.find(".pv-pano").width();height=i.find(".pv-pano").height();var t="no-repeat";if(r.repeat==true){t="repeat"}i.find(".pv-inner").css({height:height,width:width,background:"url("+imgSrc+") "+t,"background-size":"cover"});if(r.overlay==true){e("<div class='pv-overlay'><i class='pvicon-overlay'></i></div>").appendTo(i.find(".pv-inner"));i.find(".pv-inner").bind("mouseenter",function(){e(this).find(".pv-overlay ").fadeOut("fast")}).bind("mouseleave",function(){e(this).find(".pv-overlay ").fadeIn("fast")})}var n=i.find(".pv-inner"),s={w:parseInt(n.parent().width()),h:parseInt(n.parent().height())},o={w:width-s.w,h:height-s.h},u={x:0,y:0},a={x:0,y:0},f=false;n.bind("mousedown mouseup mouseleave",c);n.bind("dblclick",h);i.find(".pv-pano").hide()})})}}(window.jQuery)
+/* ===========================================================
+ * jquery-panorama_viewer.js v1
+ * ===========================================================
+ * Copyright 2014 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * Embed Panorama Photos on your website
+ * with Panorama Viewer
+ *
+ * https://github.com/peachananr/panorama_viewer
+ *
+ * ========================================================== */
+
+!function($){
+
+  var defaults = {
+    repeat: false,
+    direction: "horizontal",
+		animationTime: 700,
+		easing: "ease-out",
+		overlay: true
+	};
+
+  function Timer(callback, delay) {
+      var timerId, start, remaining = delay;
+
+      this.pause = function() {
+          window.clearTimeout(timerId);
+          remaining -= new Date() - start;
+      };
+
+      this.resume = function() {
+          start = new Date();
+          timerId = window.setTimeout(callback, remaining);
+      };
+
+      this.resume();
+  }
+
+	function touchHandler(event)
+  {
+      var touches = event.changedTouches,
+          first = touches[0],
+          type = "";
+
+           switch(event.type)
+      {
+          case "touchstart": type = "mousedown"; break;
+          case "touchmove":  type="mousemove"; break;
+          case "touchend":   type="mouseup"; break;
+          default: return;
+      }
+
+      var simulatedEvent = document.createEvent("MouseEvent");
+
+      var mult = 2;
+
+      if( navigator.userAgent.match(/Android/i) ) {
+          mult = 10
+      }
+
+      simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                                first.screenX, first.screenY,
+                                (first.clientX * mult), (first.clientY * mult), false,
+                                false, false, false, 0/*left*/, null);
+      first.target.dispatchEvent(simulatedEvent);
+  }
+  $.fn.panorama_viewer = function(options){
+
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+
+    return this.each(function(){
+      var settings = $.extend({}, defaults, options),
+      el = $(this);
+
+      el.find("> img").load(function () {
+        el.find("> img").addClass("pv-pano");
+        el.addClass("pv-container").wrapInner("<div class='pv-inner pv-animating'></div>");
+
+        if (settings.direction == "vertical") {
+           el.addClass("pv-vertical")
+        }
+
+        el.find(".pv-animating").css({
+          "-webkit-transition": "all " + settings.animationTime + "ms " + settings.easing,
+          "-moz-transition": "all " + settings.animationTime + "ms " + settings.easing,
+          "-ms-transition": "all " + settings.animationTime + "ms " + settings.easing,
+          "transition": "all " + settings.animationTime + "ms " + settings.easing
+        })
+        imgSrc = el.find(".pv-pano").attr("src")
+        width = el.find(".pv-pano").width()
+        height = el.find(".pv-pano").height()
+        var repeat = "no-repeat";
+        if (settings.repeat == true) {
+          repeat = "repeat"
+        }
+
+        el.find(".pv-inner").css({
+          height: height,
+          width: width,
+          background: "url(" + imgSrc + ") " + repeat,
+          "background-size": "cover"
+        })
+
+        if (settings.overlay == true) {
+          $("<div class='pv-overlay'><i class='pvicon-overlay'></i></div>").appendTo(el.find(".pv-inner"))
+
+          el.find(".pv-inner").bind("mouseenter", function() {
+            $(this).find(".pv-overlay ").fadeOut("fast");
+          }).bind("mouseleave", function() {
+            $(this).find(".pv-overlay ").fadeIn("fast");
+          })
+        }
+
+
+      	var $bg = el.find(".pv-inner"),
+        elbounds = {
+          w: parseInt($bg.parent().width()),
+          h: parseInt($bg.parent().height())
+        },
+        bounds = {w: width - elbounds.w, h: height - elbounds.h},
+        origin = {x: 0, y: 0},
+        start = {x: 0, y: 0},
+        movecontinue = false;
+
+        function move (e){
+
+          var inbounds = {x: false, y: false},
+              offset = {
+                  x: start.x - (origin.x - e.clientX),
+                  y: start.y - (origin.y - e.clientY)
+              };
+          if (settings.direction == "horizontal") {
+            if (settings.repeat == true) {
+              inbounds.x = true;
+            } else {
+              inbounds.x = offset.x < 0 && (offset.x * -1) < bounds.w;
+            }
+
+            if (movecontinue && inbounds.x) {
+                start.x = offset.x;
+                start.y = 0;
+            }
+          } else {
+            if (settings.repeat == true) {
+              inbounds.y = true;
+            } else {
+              inbounds.y = offset.y < 0 && (offset.y * -1) < bounds.h;
+            }
+
+            if (movecontinue && inbounds.y) {
+                start.y = offset.y;
+                start.x = 0;
+            }
+          }
+
+          $(this).css('background-position', start.x + 'px ' + start.y + 'px');
+
+
+          origin.x = e.clientX;
+          origin.y = e.clientY;
+
+          e.stopPropagation();
+          return false;
+        }
+
+        function handle (e){
+            movecontinue = false;
+            $bg.unbind('mousemove', move);
+
+            if (e.type == 'mousedown') {
+                origin.x = e.clientX;
+                origin.y = e.clientY;
+                movecontinue = true;
+                $bg.bind('mousemove', move);
+            } else {
+              $(document.body).focus();
+            }
+
+            e.stopPropagation();
+            return false;
+        }
+
+        function reset (){
+            start = {x: 0, y: 0};
+            $(this).css('backgroundPosition', '0 0');
+        }
+
+
+        $bg.bind('mousedown mouseup mouseleave', handle);
+        $bg.bind('dblclick', reset);
+
+        el.find(".pv-pano").hide()
+      }).each(function(){
+        if (this.complete || this.complete === undefined){ $(this).load(); } //needed for potential cached images
+      });
+
+
+    });
+
+  }
+
+}(window.jQuery);
