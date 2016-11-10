bce59cec823061ff4a628bc32f15c21f2be41409
Muhammad Khan <muhammadkh4n@gmail.com>
2016-11-10 19:08:42 +0500

    added record feature to slider

--- app/assets/javascripts/menu-slider.js
+++ app/assets/javascripts/menu-slider.js
@@ -1,27 +1,40 @@
 $(document).ready(function() {
   // slider menu
   if ($('.menu-slider').length) {
-    var list = $('.menu-slider .menu-list');
-    var initialPos = list.position().top;
-    var pos = list.position().top;
-    var startPos = $('#startPos').position().top;
-    var endPos = $('#endPos').position().top;
-    var menuSlider = setInterval(function() {
-      pos -= 1;
-      list.css({top: pos});
-      if (endPos <= startPos) {
-        list.css({top: initialPos});
-        pos = initialPos;
-        endPos = $('#endPos').position().top;
-      }
-      endPos--;
-    }, 40);
+    function slider(stopAtEnd) {
+      var list = $('.menu-slider .menu-list');
+      var initialPos = list.position().top;
+      var pos = list.position().top;
+      var startPos = $('#startPos').position().top;
+      var endPos = $('#endPos').position().top;
+      var interval = setInterval(function() {
+        pos -= 1;
+        list.css({top: pos});
+        if (endPos <= startPos) {
+          list.css({top: initialPos});
+          pos = initialPos;
+          endPos = $('#endPos').position().top;
+          if (stopAtEnd)
+            clearInterval(interval);
+        }
+        endPos--;
+      }, 40);
+      return interval;
+    }
 
-    // clearInterval(menuSlider);
+    var s = null;
 
-    // fullscreen button
-    $("#menu-full").click(function() {
+    $('#menu-full').click(function () {
       $('.menu-slider').fullScreen(true);
+      s = slider(false);
     });
+
+    $('#menu-record').click(function () {
+      $('.menu-slider').fullScreen(true);
+      setTimeout(function () {
+        clearInterval(s);
+        s = slider(true);
+      }, 5000);
+    })
   }
 });
